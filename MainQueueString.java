c50e0985e30e7a4a7554274a3bbcf410376d8e78
WANTHYE <44660843+WANTHYE@users.noreply.github.com>
2018-11-01 12:33:08 +0700

    Create MainQueueString.java

---
+++ MainQueueString.java
@@ -0,0 +1,17 @@
+
+public class MainQueueString {
+
+
+	public static void main(String[] args) {
+		QueueString lopiu = new QueueString();
+		lopiu.cetak();
+		
+		lopiu.insert("Sudarminkuh");lopiu.cetak();
+		lopiu.insert("adalah");lopiu.cetak();
+		lopiu.insert("calon");lopiu.cetak();
+		lopiu.insert("hokage");lopiu.cetak();
+
+	}
+
+}
+
