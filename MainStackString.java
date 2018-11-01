e280a7145922d6d18bad280252a203983388eb9b
WANTHYE <44660843+WANTHYE@users.noreply.github.com>
2018-11-01 13:02:23 +0700

    Create MainStackString.java

---
+++ MainStackString.java
@@ -0,0 +1,20 @@
+
+public class MainStackString {
+
+		public static void main(String[] args) {
+			StackString irwanti = new StackString();
+					
+				irwanti.push(" ini");
+			    irwanti.push("adalah");
+				irwanti.push("saya");
+				irwanti.push("tadaaaa");
+					
+					String d0=irwanti.pop();
+					System.out.println(d0);
+					irwanti.cetak();
+					
+									
+				}
+
+			}
+
