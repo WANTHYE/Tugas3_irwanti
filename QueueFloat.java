85c2a4f7c69ed72d6f8e6029c0d30e28b2e8d86d
WANTHYE <44660843+WANTHYE@users.noreply.github.com>
2018-11-01 13:05:58 +0700

    Create QueueFloat.java

---
+++ QueueFloat.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+
+public class QueueFloat {
+ArrayList<Float> sudarminkuh = new ArrayList<Float>();
+	int sudarminbanget = -1;
+
+	public void insert(Float value) {
+		sudarminbanget++;
+		sudarminkuh.add(sudarminbanget,value);
+	}
+	public Float get() {
+		Float value = 0.6f;
+		if(sudarminbanget>-1) {
+			value = sudarminkuh.get(0);
+			sudarminkuh.remove(0);
+			sudarminbanget--;
+		}
+		return value;
+		
+	}
+	public void cetak1() {
+		System.out.println("Step : "+sudarminbanget+" > VARIABLE: "+sudarminkuh.toString());
+	}
+	public void cetak() {
+		// TODO Auto-generated method stub
+		
+	}
+}
